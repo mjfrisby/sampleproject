9099b80febb4576b949e3f4b673a98ba9acc4b5b
mjfrisby <98243082+mjfrisby@users.noreply.github.com>
2023-08-22 12:45:20 -0400

    Add one line to wip_1 after new file was added to wip_1_1 branch

--- src/sample/__init__.py
+++ src/sample/__init__.py
@@ -3,4 +3,6 @@ def main():
     print("Call your main application code here")
     print("Add one line to main branch")
     print("Add one line to wip_1 branch")
+    print("Add line to wip_1 after wip_1_1 new file was created")
+
 
