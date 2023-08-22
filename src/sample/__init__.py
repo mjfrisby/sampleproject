9370356c60078317c599033e698c281e462e185c
mjfrisby <98243082+mjfrisby@users.noreply.github.com>
2023-08-22 12:42:37 -0400

    Add one line to wip_1 branch

--- src/sample/__init__.py
+++ src/sample/__init__.py
@@ -2,3 +2,5 @@ def main():
     """Entry point for the application script"""
     print("Call your main application code here")
     print("Add one line to main branch")
+    print("Add one line to wip_1 branch")
+
