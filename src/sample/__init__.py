e17e151bf363baded49731b7ff84486b381f241f
mjfrisby <98243082+mjfrisby@users.noreply.github.com>
2023-08-22 13:07:57 -0400

    add a line post wip1_2 new file

--- src/sample/__init__.py
+++ src/sample/__init__.py
@@ -5,6 +5,7 @@ def main():
     print("Add one line to wip_1 branch")
     print("Add line to wip_1 after wip_1_1 new file was created")
     print("add a line post wip1_2 new file")
+    print("make a change post wip1->wip_1_2 merge")
 
 
 
