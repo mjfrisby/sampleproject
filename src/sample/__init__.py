df9522eb04e067943cfb4cd33b04c7393e677fb7
mjfrisby <98243082+mjfrisby@users.noreply.github.com>
2024-01-06 10:53:20 -0500

    change line

--- src/sample/__init__.py
+++ src/sample/__init__.py
@@ -1,4 +1,4 @@
 def main():
     """Entry point for the application script"""
     print("Call your main application code here")
-    print("Add one line to main branch")
+    print("this is a changed line")
